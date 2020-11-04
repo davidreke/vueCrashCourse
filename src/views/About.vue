73bf208556bf69be942ad0faab1a5487e31dc8cd
davidreke <54542172+davidreke@users.noreply.github.com>
2020-11-04 10:23:09 -0600

    adding dist

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
     <h1>About</h1>
-    <p>This is the about page</p>
+    <p>This is the about page for the Brad Traversey Vue crash course.</p>
   </div>
 </template>
